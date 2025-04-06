4ecc921b31541391ba220e8601f9ed63cce01d01
harikesh0402 <harikeshwatmare77@gmail.com>
2025-04-07 01:20:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-batch-27-harry"
+    bucket = "young-minds-app-batch-27-harry-1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
